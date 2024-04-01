81fae1a6df4186788a07b061dd226c9a69d6a989
TejaeW11 <114426336+TejaeW11@users.noreply.github.com>
2024-03-31 23:25:33 -0400

    Add files via upload

--- src/main/java/claims/models/Drivers/ClaimsDatabaseDriver.java
+++ src/main/java/claims/models/Drivers/ClaimsDatabaseDriver.java
@@ -1,10 +1,10 @@
 package claims.models.Drivers;
 
 import java.sql.Statement;
-import java.time.LocalDate;
 
 import claims.models.Customer;
 import claims.models.NewUser;
+import claims.models.Policy;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -36,81 +36,23 @@ public class ClaimsDatabaseDriver {
     
     
     
-    public void addCustomer(String username, String password, String firstName, String lastName, int age, String sex, String email, String phone, String address, String DOB) {
-        String sql = "INSERT INTO Customers (Username, Password, FirstName, LastName, Age, Sex, Email, Phone, Address, DOB) " +
-                     "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-                     try (PreparedStatement pstmt = this.conn.prepareStatement(sql)) {
-                        pstmt.setString(1, username);
-                        pstmt.setString(2, password);
-                        pstmt.setString(3, firstName);
-                        pstmt.setString(4, lastName);
-                        pstmt.setInt(5, age);
-                        pstmt.setString(6, sex);
-                        pstmt.setString(7, email);
-                        pstmt.setString(8, phone);
-                        pstmt.setString(9, address);
-                        pstmt.setString(10, DOB);
-                        int affectedRows = pstmt.executeUpdate();
-                        
-                        if (affectedRows > 0) {
-                            System.out.println("Update successful.");
-                        } else {
-                            System.out.println("Update failed. No rows affected.");
-                        }
-                    } catch (SQLException e) {
-                        e.printStackTrace();
-                    }
-	}
+    public void addCustomer(NewUser user) {
 
-    public void addAdvisor(String username, String password, String firstName, String lastName, String sex, String email, String phone, String address, String DOB) {
-        String sql = "INSERT INTO Advisors (Username, Password, FirstName, LastName, Sex, Email, Phone, Address, DOB) " +
-                     "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
-                     try (PreparedStatement pstmt = this.conn.prepareStatement(sql)) {
-                        pstmt.setString(1, username);
-                        pstmt.setString(2, password);
-                        pstmt.setString(3, firstName);
-                        pstmt.setString(4, lastName);
-                        pstmt.setString(5, sex);
-                        pstmt.setString(6, email);
-                        pstmt.setString(7, phone);
-                        pstmt.setString(8, address);
-                        pstmt.setString(9, DOB);
-                        int affectedRows = pstmt.executeUpdate();
-                        
-                        if (affectedRows > 0) {
-                            System.out.println("Update successful.");
-                        } else {
-                            System.out.println("Update failed. No rows affected.");
-                        }
-                    } catch (SQLException e) {
-                        e.printStackTrace();
-                    }
+    	String sql = "INSERT INTO Customers"
+				+ "(Username, Password, Firstname, Lastname, Age,Sex,Email,Phone,Address)\r\n"
+				+ "VALUES ('"+user.getUsername()+"', '"+user.getPasswordKey()+"', '"+user.getFirstName()+"', '"+user.getLastName()+"',"
+						+ " '"+user.getAge()+"', '"+user.getGender()+"', '"+user.getEmail()+"', "+user.getPhone()+", '"+user.getAddr()+"')";
+    	
+		
+		try {
+			Statement st = conn.createStatement();
+			int rs = st.executeUpdate(sql);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
 	}
  
-    public void addAdmin(String username, String password, String firstName, String lastName, String sex, String email, String phone, String address, String DOB) {
-        String sql = "INSERT INTO Admins (Username, Password, FirstName, LastName, Sex, Email, Phone, Address, DOB) " +
-                     "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
-                     try (PreparedStatement pstmt = this.conn.prepareStatement(sql)) {
-                        pstmt.setString(1, username);
-                        pstmt.setString(2, password);
-                        pstmt.setString(3, firstName);
-                        pstmt.setString(4, lastName);
-                        pstmt.setString(5, sex);
-                        pstmt.setString(6, email);
-                        pstmt.setString(7, phone);
-                        pstmt.setString(8, address);
-                        pstmt.setString(9, DOB);
-                        int affectedRows = pstmt.executeUpdate();
-                        
-                        if (affectedRows > 0) {
-                            System.out.println("Update successful.");
-                        } else {
-                            System.out.println("Update failed. No rows affected.");
-                        }
-                    } catch (SQLException e) {
-                        e.printStackTrace();
-                    }
-	}
     
     public ResultSet searchAllCustomers(){
         Statement statement;
@@ -124,15 +66,11 @@ public class ClaimsDatabaseDriver {
         return resultSet;
     }
 
-    public ResultSet searchCustomerByAdvisorID(int ID) {
-        String sql = "SELECT DISTINCT Customers.* " +
-                 "FROM Customers " +
-                 "JOIN Claims ON Customers.ClientID = Claims.ClientID " +
-                 "WHERE Claims.AdvisorID = ?";
+    public ResultSet searchCustomerByAdvisorID() {
         ResultSet resultSet = null;
-        try (PreparedStatement pstmt = this.conn.prepareStatement(sql)){
-            pstmt.setInt(1, ID);
-             resultSet = pstmt.executeQuery();  
+        // Use try-with-resources to ensure proper closure of resources
+        try (PreparedStatement preparedStatement = this.conn.prepareStatement("SELECT * FROM Customers")) {
+            resultSet = preparedStatement.executeQuery();
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -190,32 +128,6 @@ public class ClaimsDatabaseDriver {
     // public void registerClaim(String )
 
     //Advisor Section
-
-    public void updateAdvisor(String username, String password, String firstName, String lastName, String DOB, String sex, String email, String phone, String address, int advisorId) {
-        String sql = "UPDATE Advisors SET Username=?, Password=?, FirstName=?, LastName=?, " +
-        "DOB=?, Sex=?, Email=?, Phone=?, Address=? WHERE AdvisorID=?";
-        try (PreparedStatement pstmt = this.conn.prepareStatement(sql)) {
-            pstmt.setString(1, username);
-            pstmt.setString(2, password);
-            pstmt.setString(3, firstName);
-            pstmt.setString(4, lastName);
-            pstmt.setString(5, DOB);
-            pstmt.setString(6, sex);
-            pstmt.setString(7, email);
-            pstmt.setString(8, phone);
-            pstmt.setString(9, address);
-            pstmt.setInt(10, advisorId);
-            int affectedRows = pstmt.executeUpdate();
-            
-            if (affectedRows > 0) {
-                System.out.println("Update successful.");
-            } else {
-                System.out.println("Update failed. No rows affected.");
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
     
     public void addAdvisor(NewUser user) {
 
@@ -317,24 +229,12 @@ public class ClaimsDatabaseDriver {
         return resultSet;
     }
     
-    public ResultSet getClaimDetailsByClient(int clientID){
+    public ResultSet getClaimDetails(int claimID){
         Statement statement;
         ResultSet resultSet = null;
         try {
             statement = this.conn.createStatement();
-            resultSet = statement.executeQuery("SELECT * FROM Claims WHERE ClientID="+clientID+";");  
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return resultSet;
-    }
-
-    public ResultSet getClaimDetailsByAdvisor(int advisorID){
-        Statement statement;
-        ResultSet resultSet = null;
-        try {
-            statement = this.conn.createStatement();
-            resultSet = statement.executeQuery("SELECT * FROM Claims WHERE AdvisorID="+advisorID+";");  
+            resultSet = statement.executeQuery("SELECT * FROM Claims WHERE ClaimID="+claimID+";");  
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -353,6 +253,21 @@ public class ClaimsDatabaseDriver {
 
     //Policy Section
 
+    public void addPolicy(Policy policy) {
+    	String sql = "INSERT INTO Policies"
+				+ "(PolicyNumber, StartDate, EndDate, Premium, Deductible, CoverageType, PolicyStatus, PolicyName)\r\n"
+				+ "VALUES ('"+policy.getPolicyNumber()+"', '"+policy.getStartDate()+"', '"+policy.getEndDate()+"', '"+policy.getPremium()+"','"
+						+policy.getDeductible()+"', '"+policy.getCoverageType()+"', '"+policy.getPolicyStatus()+"', '"+policy.policyName+"')";
+		
+		try {
+			Statement st = conn.createStatement();
+			int rs = st.executeUpdate(sql);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+    }
+    
+    
     public ResultSet getAllPolicies(){
         Statement statement;
         ResultSet resultSet = null;
@@ -420,84 +335,4 @@ public class ClaimsDatabaseDriver {
     }
 
 
-    public ResultSet getAllCustomers() {
-        Statement statement;
-        ResultSet resultSet = null;
-        try {
-            statement = this.conn.createStatement();
-            resultSet = statement.executeQuery("SELECT * FROM Customers");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return resultSet;
-    }
-    public void deleteCustomer(int customerID) {
-        String sql = "DELETE FROM Customers WHERE ClientID = ?";
-        try (
-                Connection connection = DriverManager.getConnection("jdbc:sqlite:Databases/claims.db");
-                PreparedStatement statement = connection.prepareStatement(sql)
-        ) {
-            statement.setInt(1, customerID);
-            int rowsAffected = statement.executeUpdate();
-            if (rowsAffected > 0) {
-                System.out.println("Customer with ID " + customerID + " deleted successfully.");
-            } else {
-                System.out.println("Customer with ID " + customerID + " not found.");
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void deleteAdvisor(int advisorID) {
-        String sql = "DELETE FROM Advisors WHERE AdvisorID = ?";
-        try (
-                Connection connection = DriverManager.getConnection("jdbc:sqlite:Databases/claims.db");
-                PreparedStatement statement = connection.prepareStatement(sql)
-        ) {
-            statement.setInt(1, advisorID);
-            int rowsAffected = statement.executeUpdate();
-            if (rowsAffected > 0) {
-                System.out.println("Advisor with ID " + advisorID + " deleted successfully.");
-            } else {
-                System.out.println("Advisor with ID " + advisorID + " not found.");
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void resetAdvisorPassword(int userID) {
-        String newPassword = "password";
-        String sql = "UPDATE Advisors SET Password = ? WHERE AdvisorID = ?";
-        try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
-            pstmt.setString(1, newPassword);
-            pstmt.setInt(2, userID);
-            int affectedRows = pstmt.executeUpdate();
-            if (affectedRows > 0) {
-                System.out.println("Password reset for advisor with ID " + userID + " successful.");
-            } else {
-                System.out.println("Advisor with ID " + userID + " not found.");
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void resetCustomerPassword(int userID) {
-        String newPassword = "password";
-        String sql = "UPDATE Customers SET Password = ? WHERE ClientID = ?";
-        try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
-            pstmt.setString(1, newPassword);
-            pstmt.setInt(2, userID);
-            int affectedRows = pstmt.executeUpdate();
-            if (affectedRows > 0) {
-                System.out.println("Password reset for customer with ID " + userID + " successful.");
-            } else {
-                System.out.println("Customer with ID " + userID + " not found.");
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
 }
